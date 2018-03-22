e9abcc1b57116c37c6c39114a5bfd661432f1f09
fengqiao <wh1232005@21cn.com>
2018-03-22 23:45:47 +0800

    增加uri参数的正则匹配,暂时在get方法调试

--- lua-restful/app.lua
+++ lua-restful/app.lua
@@ -25,10 +25,29 @@ request.body = post
 
 
 --get方法
-function _M:get(uri,func)
-	local _uri = string.lower(ngx.var.uri)
+function _M:get(rule,func)
+	local _uri = string.lower(ngx.var.uri) 
 	
-	if  string.match(_uri,uri) == _uri and string.lower(ngx.var.request_method) == 'get' then 
+
+    local _rule1 = string.gsub(rule, ':d','%%d+') 
+	local _rule1 = string.gsub(_rule1, ':w','%%w+')
+	local _rule1 = string.gsub(_rule1, ':a','%%a+')
+	if  string.match(_uri,_rule1) == _uri and string.lower(ngx.var.request_method) == 'get' then 
+	
+		--统计参数个数
+		_,num = string.gsub(rule, ':','')
+		local str = "{"
+		for i=1,num do
+		  str = str.."["..i.."]='%"..i.."',"
+		end
+		str = string.sub(str, 1, -2)..'}'
+		--把uri参数输出table格式
+		local _rule = string.gsub(rule, ':d','(%%d+)')
+		local _rule = string.gsub(_rule, ':w','(%%w+)')
+		local _rule = string.gsub(_rule, ':a','(%%a+)')
+		local _table = string.gsub(_uri,_rule,str)
+		request.params = StrToTable(_table)
+	 
 		if func then
 			func(request)
 		end
@@ -73,6 +92,46 @@ function _M:put(uri,func)
 			func(request)
 		end
 	end
+end
+
+--字符串转table
+function StrToTable(str)
+    if str == nil or type(str) ~= "string" then
+        return
+    end
+    
+    return loadstring("return " .. str)()
+end
+
+--打印table
+function PrintTable( tbl , level, filteDefault)
+  local msg = ""
+  filteDefault = filteDefault or true --默认过滤关键字（DeleteMe, _class_type）
+  level = level or 1
+  local indent_str = ""
+  for i = 1, level do
+    indent_str = indent_str.."  "
+  end
+
+  ngx.say(indent_str .. "<pre>{")
+  for k,v in pairs(tbl) do
+    if filteDefault then
+      if k ~= "_class_type" and k ~= "DeleteMe" then
+        local item_str = string.format("%s%s = %s", indent_str .. " ",tostring(k), tostring(v))
+        ngx.say(item_str)
+        if type(v) == "table" then
+          PrintTable(v, level + 1)
+        end
+      end
+    else
+      local item_str = string.format("%s%s = %s", indent_str .. " ",tostring(k), tostring(v))
+      ngx.say(item_str)
+      if type(v) == "table" then
+        PrintTable(v, level + 1)
+      end
+    end
+  end
+  ngx.say(indent_str .. "}")
 end
 
 return _M
--- lua-restful/index.lua
+++ lua-restful/index.lua
@@ -7,3 +7,12 @@ app:post('/user/[a-z0-9]+',function(request)
 	ngx.say(request.body.a)
 	ngx.say(request.body.b)
 end)
+
+app:get('/a/:d/b/:d/c/:d/d/:a/:w',function(request)
+	 ngx.say('1')
+end)
+
+app:get('/blog/:d',function(request)
+	  ngx.say('blog')
+	  ngx.say(request.params[1])
+end)
