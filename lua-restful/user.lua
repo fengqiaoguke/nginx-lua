3fe9bff7b619f270408dcf4c7b01ecb8eba77b94
fengqiao <fengqiao@fengqiaoju.com>
2018-04-25 18:08:26 +0800

    1

--- lua-restful/user.lua
+++ lua-restful/user.lua
@@ -5,7 +5,7 @@ local head = ngx.req.get_headers()
  
 --  
 function _M:checkToken() 
-	local utoken = 1
+	local utoken = 1 
 	local timestamp = head['timestamp'] or ''
 	local sign = ngx.md5(ngx.var.request_uri .. '@'.. utoken ..'#'..timestamp)
 	if head['sign'] ~= sign then
